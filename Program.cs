c82614b593ae60283b0cd15bfe1e1cb78646e239
Albinotektek <162760294+Albinotektek@users.noreply.github.com>
2024-04-07 19:48:20 -0300

    Rolagem de dados

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿﻿using System.Security.Cryptography;
+
+Console.WriteLine();
+
+Console.WriteLine("Rolagem de dados");
+
+Console.WriteLine();
+
+Console.Write("Digite quantas faces tem o dado:");
+int faces = Convert.ToInt32(Console.ReadLine());
+
+int rolagem = RandomNumberGenerator.GetInt32(1, faces +1);
+
+Console.WriteLine();
+
+Console.WriteLine($"A rolagem do dado resultou em {rolagem}");
+Console.WriteLine();
